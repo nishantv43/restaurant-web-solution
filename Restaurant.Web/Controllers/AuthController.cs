322ec0faa9afde838503a955f7188e67b75d2c2c
nishu vimal <vimalnishant43@gmail.com>
2023-10-11 18:31:38 +0100

    Added Screens for Login and Register

---
+++ Restaurant.Web/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Restaurant.Web.Models;
+using Restaurant.Web.Service.IService;
+using Restaurant.Web.Utility;
+
+namespace Restaurant.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDto loginRequestDto = new();
+            return View(loginRequestDto);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem{Text= SD.RoleAdmin, Value= SD.RoleAdmin},
+                new SelectListItem{Text= SD.RoleCustomer, Value = SD.RoleCustomer}
+            };
+            ViewBag.RoleList = roleList;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            return View();
+        }
+
+    }
+}
