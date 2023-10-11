18523dd6e2b4219e4782278f6e01af86f2b2cd82
nishu vimal <vimalnishant43@gmail.com>
2023-10-11 18:08:13 +0100

    Adding AuthService in the Client

---
+++ Restaurant.Web/Service/IService/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Restaurant.Web.Models;
+
+namespace Restaurant.Web.Service.IService
+{
+    public interface IAuthService
+    {
+        Task<ResponseDto?> LoginAsync(LoginRequestDto loginRequestDto);
+        Task<ResponseDto?> RegisterAsync(RegistrationRequestDto registrationRequestDto);
+        Task<ResponseDto?> AssignRoleAsync(RegistrationRequestDto registrationRequestDto);
+    }
+}
