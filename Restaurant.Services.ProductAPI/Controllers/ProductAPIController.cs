3cd20b86f498f390452aa6ac4763c794caa6dbc7
nishu vimal <vimalnishant43@gmail.com>
2023-10-13 17:51:25 +0100

    Updated the HomePage

--- Restaurant.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ Restaurant.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -10,7 +10,7 @@ namespace Restaurant.Services.ProductAPI.Controllers
 {
     [Route("api/product")]
     [ApiController]
-    [Authorize]
+    //[Authorize]
     public class ProductAPIController : ControllerBase
     {
         private readonly AppDbContext _db;
