d402f2234c3bfff442f9369870772c1683358d31
nishu vimal <vimalnishant43@gmail.com>
2023-10-15 19:01:59 +0100

    Added ShoppingCart Project

---
+++ Restaurant.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Restaurant.Services.ShoppingCartAPI.Data;
+using Restaurant.Services.ShoppingCartAPI.Models;
+using Restaurant.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Restaurant.Services.ShoppingCartAPI.Controllers
+{
+    [Route("api/cart")]
+    [ApiController]
+    public class CartAPIController : ControllerBase
+    {
+        private ResponseDto _response;
+        private IMapper _mapper;
+        private readonly AppDbContext _db;
+        //private IProductService _productService;
+        //private ICouponService _couponService;
+        //private IConfiguration _configuration;
+        //private readonly IMessageBus _messageBus;
+        public CartAPIController(AppDbContext db,
+            IMapper mapper)
+        {
+            _db = db;
+            _response = new ResponseDto();
+            _mapper = mapper;
+
+        }
+
+        [HttpPost("ModifyCart")]
+        public async Task<ResponseDto> ModifyCart(CartDto cartDto)
+        {
+            try
+            {
+                var cartHeaderFromDb = await _db.CartHeaders.AsNoTracking()
+                    .FirstOrDefaultAsync(u => u.UserId == cartDto.CartHeader.UserId);
+
+                if (cartHeaderFromDb == null)
+                {
+                    //create header and details
+                    CartHeader cartHeader = _mapper.Map<CartHeader>(cartDto.CartHeader);
+
+                    _db.CartHeaders.Add(cartHeader);
+                    await _db.SaveChangesAsync();
+
+                    cartDto.CartDetails.First().CartHeaderId = cartHeader.CartHeaderId;
+
+                    _db.CartDetails.Add(_mapper.Map<CartDetails>(cartDto.CartDetails.First()));
+                    await _db.SaveChangesAsync();
+                }
+                else
+                {
+                    //if header is not null
+                    //check if details has same product
+                    var cartDetailsFromDb = await _db.CartDetails.AsNoTracking().
+                        FirstOrDefaultAsync(u => u.ProductId == cartDto.CartDetails.First().ProductId 
+                        && u.CartHeaderId == cartHeaderFromDb.CartHeaderId);
+
+                    if (cartDetailsFromDb == null)
+                    {
+                        //create cartdetails
+                        cartDto.CartDetails.First().CartHeaderId = cartHeaderFromDb.CartHeaderId;
+
+                        _db.CartDetails.Add(_mapper.Map<CartDetails>(cartDto.CartDetails.First()));
+                        await _db.SaveChangesAsync();
+                    }
+
+                    else
+                    {
+                        //update count in cart details
+                        cartDto.CartDetails.First().Count += cartDetailsFromDb.Count;
+                        cartDto.CartDetails.First().CartHeaderId = cartDetailsFromDb.CartHeaderId;
+                        cartDto.CartDetails.First().CartDetailsId = cartDetailsFromDb.CartDetailsId;
+
+                        _db.CartDetails.Update(_mapper.Map<CartDetails>(cartDto.CartDetails.First()));
+                        await _db.SaveChangesAsync();
+                    }
+                }
+                _response.Result = cartDto;
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.Message.ToString();
+                _response.IsSuccess = false;
+            }
+            return _response;
+        }
+
+    }
+}
---
+++ Restaurant.Services.ShoppingCartAPI/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurant.Services.ShoppingCartAPI.Models;
+
+namespace Restaurant.Services.ShoppingCartAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CartHeader> CartHeaders { get; set; }
+        public DbSet<CartDetails> CartDetails { get; set; }
+
+    }
+}
