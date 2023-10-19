cb079c5b5b8ab271ec5ee18ef3094a14eb8142cd
nishu vimal <vimalnishant43@gmail.com>
2023-10-19 15:22:48 +0100

    Added Consumer to recieve messages from AzureBus

---
+++ Restaurant.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
@@ -0,0 +1,66 @@
+﻿using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using Restaurant.Services.EmailAPI.Models.Dto;
+using System.Text;
+
+namespace Restaurant.Services.EmailAPI.Messaging
+{
+    public class AzureServiceBusConsumer : IAzureServiceBusConsumer
+    {
+        private readonly string serviceBusConnectionString;
+        private readonly string emailCartQueue;
+        private readonly IConfiguration _configuration;
+        private ServiceBusProcessor _emailBusProcessor;
+
+        public AzureServiceBusConsumer(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            serviceBusConnectionString = _configuration.GetValue<string>("ServiceBusConnectionString");
+
+            emailCartQueue = _configuration.GetValue<string>("TopicAndQueueNames:EmailShoppingCartQueue");
+
+            var client = new ServiceBusClient(serviceBusConnectionString);
+
+            _emailBusProcessor = client.CreateProcessor(emailCartQueue);
+        }
+
+        public async Task Start()
+        {
+            _emailBusProcessor.ProcessMessageAsync += OnEmailCartRequestReceived;
+            _emailBusProcessor.ProcessErrorAsync += ErrorHandler;
+            _emailBusProcessor.StartProcessingAsync();
+        }
+        public async Task Stop()
+        {
+            await _emailBusProcessor.StopProcessingAsync();
+            await _emailBusProcessor.DisposeAsync();
+        }
+
+        private Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task OnEmailCartRequestReceived(ProcessMessageEventArgs args)
+        {
+            var message = args.Message;
+            var body = Encoding.UTF8.GetString(message.Body);
+
+            CartDto objMessage = JsonConvert.DeserializeObject<CartDto>(body);
+            try
+            {
+                //TODO - try to log email
+                //await _emailService.EmailCartAndLog(objMessage);
+                await args.CompleteMessageAsync(args.Message);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+
+    }
+}
