317446ff0652ba3c201fffe40e1ed2874f34412e
nishu vimal <vimalnishant43@gmail.com>
2023-10-19 14:39:53 +0100

    Created New Microservice for sending Email to Azure Bus

---
+++ Restaurant.Services.EmailAPI/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurant.Services.EmailAPI.Models;
+
+namespace Restaurant.Services.EmailAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<EmailLogger> EmailLoggers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
