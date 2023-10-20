cdfc5b210e68f6dd81c2dd0fce98c56bb0e53c28
nishu vimal <vimalnishant43@gmail.com>
2023-10-20 16:22:08 +0100

    Added a topic with 2 queues in Azure Bus to send msg once order is placed

---
+++ Restaurant.Services.RewardsAPI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurant.Services.RewardsAPI.Models;
+
+namespace Restaurant.Services.RewardsAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Rewards> Orders { get; set; }
+
+    }
+}
