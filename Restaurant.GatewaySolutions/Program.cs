e3db047d2b38f355d9e23cce539321904440a21a
nishu vimal <vimalnishant43@gmail.com>
2023-10-22 16:41:04 +0100

    Implmented gateway with Ocelot

---
+++ Restaurant.GatewaySolutions/Program.cs
@@ -0,0 +1,19 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Restaurant.GatewaySolutions.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddAppAuthentication();
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange : true);
+
+builder.Services.AddOcelot(builder.Configuration);
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.UseOcelot().GetAwaiter().GetResult();
+
+app.Run();
